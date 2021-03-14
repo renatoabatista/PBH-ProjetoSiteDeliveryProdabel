a29ae38d34717a4a81c9e38e69e6f8d5c452bec1
renatoabatista <renatoabatista@gmail.com>
2021-03-13 21:22:02 -0300

    inclusão do cupom de desconto

--- JS/modulos/cupom.js
+++ JS/modulos/cupom.js
@@ -1,11 +1,33 @@
 function ativarCupom(){
     let cupomDigitadoAtivado = document.getElementById('cupom').value
-    let cupom = ['#10', '#20', 'Teste', '#RENATO', 'OK', 'ok', 'prodabel']
-
+    let valorCupom = 0;
+    let cupom = ['#10', '#20', '#RENATO', 'PRIMEIRACOMPRA','PRODABEL', 'MONTREAL']
     for(let i = 0; i < cupom.length; i++){
         if(cupom[i] == cupomDigitadoAtivado){
-            console.log("achei")
-            document.getElementById('cupom').value = `CUPOM DE :R$ ${cupom[i]} ATIVADO`
+            switch(cupom[i]){
+                case '#10':
+                valorCupom = 10;
+                break
+                case '#20':
+                valorCupom = 20;
+                break
+                default:
+                case '#RENATO':
+                valorCupom = 50;
+                break
+                case 'PRIMEIRACOMPRA':
+                valorCupom = 15;    
+                break
+                case 'prodabel':
+                valorCupom = 60;
+                break
+                case 'Montreal':
+                valorCupom = 100;
+                break
+            }
+            document.getElementById('cupom').value = `Ganhou R$ ${valorCupom.toFixed(2).replace('.',',')}`
+            document.getElementById('cupom').disabled = true 
+            return valorCupom;
         }
     }
 }
